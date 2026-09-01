38ee5dcd03a27c81eb9a90faf4594f62465d098e
Lucca Dutra <luccacsdutra@gmail.com>
2026-09-01 14:59:18 -0300

    Add files via upload

---
+++ Cadastro de veiculo/Cadastro de veiculo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_veiculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
