72ec6829b55abf6d59337754a853461cf852e6c6
Lucca Dutra <luccacsdutra@gmail.com>
2026-09-01 14:29:10 -0300

    Add files via upload

---
+++ cadsastro do filme/cadsastro do filme/Module1.vb
@@ -0,0 +1,45 @@
+﻿Using System;
+
+Class Program
+{
+    Static void Main()
+    {
+        Console.WriteLine("=================================");
+        Console.WriteLine("       CADASTRO DE FILME");
+        Console.WriteLine("=================================");
+
+        Console.Write("Nome do filme: ");
+        String nome = Console.ReadLine();
+
+        Console.Write("Nome do diretor: ");
+        String diretor = Console.ReadLine();
+
+        Console.Write("Ano de lançamento: ");
+        int ano = Int.Parse(Console.ReadLine());
+
+        Console.Write("Duração em minutos: ");
+        int duracao = Int.Parse(Console.ReadLine());
+
+        Console.Write("Nota do filme: ");
+        Double nota = Double.Parse(Console.ReadLine());
+
+        Console.Write("O filme já foi assistido? (true/false): ");
+        bool assistido = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("=================================");
+        Console.WriteLine("       FILME CADASTRADO");
+        Console.WriteLine("=================================");
+
+        Console.WriteLine("Filme: " + nome);
+        Console.WriteLine("Diretor: " + diretor);
+        Console.WriteLine("Ano de lançamento: " + ano);
+        Console.WriteLine("Duração: " + duracao + " minutos");
+        Console.WriteLine("Nota: " + nota.ToString("F1"));
+        Console.WriteLine("Já foi assistido: " + assistido);
+
+        Console.WriteLine();
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
---
+++ cadsastro do filme/cadsastro do filme/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("cadsastro do filme")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("cadsastro do filme")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("5e6f5a85-7e34-47df-a2d7-d08d4c3a8b51")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
